f89a2f2bccfd286dca06609475939c5f919f93d6
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-06-11 09:12:21 +0600

    approved and denied status API created

--- index.js
+++ index.js
@@ -144,6 +144,30 @@ async function run() {
     });
 
     // classes related APIs
+    app.patch("/classes/approved/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "approved",
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/classes/denied/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "denied",
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     app.get("/classes", async (req, res) => {
       let query = {};
 
